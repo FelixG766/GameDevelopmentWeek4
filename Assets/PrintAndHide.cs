b5a11567e73974f54208b17fc6cde7927650036b
Felix Guo <guoyangru@gmail.com>
2023-08-30 02:14:01 +1000

    90 percent complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -7,6 +7,7 @@ public class ConsolePrint : MonoBehaviour
     // Start is called before the first frame update
 
     private int i = 3;
+    public Renderer rend;
     void Start()
     {
         
